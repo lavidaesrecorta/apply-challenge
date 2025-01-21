ea6ad3521598b4bf3d33422e7daece06bcedc09f
Rodrigo Jimenez <jimenez.rodrigo.dev@gmail.com>
2025-01-21 08:01:55 -0300

    docs: swagger integration

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const config = new DocumentBuilder()
+    .setTitle('JavaScript Back End Developer Test')
+    .setDescription('Rodrigo Jimenez - Apply Digital - 2025')
+    .setVersion('1.0')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory);
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
